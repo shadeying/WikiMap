bc11a6c14bfd14b87895e783cdb55b19a1ab49ba
shadeying <shadeying@gmail.com>
2019-05-24 07:39:33 +0000

    Favourite button added

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -5,4 +5,20 @@ $(document).ready(function() {
     $( "input[name=pointtitle]" ).focus();
   });
 
+  let click = 0;
+  $(document).on( "click", "button.fav-button", function(event) {
+    event.preventDefault();
+    if(click === 0){
+      click ++;
+    }else{
+      click --;
+    }
+
+    if(click === 1){
+      document.getElementsByClassName("fav-button")[0].style.color = "#FE938C";
+    }else{
+      document.getElementsByClassName("fav-button")[0].style.color = "#5D576B";
+    }
+  });
+
 });
