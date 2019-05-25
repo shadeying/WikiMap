29d07280fa165daa36f08ffbc91c329ddfbf1153
shadeying <shadeying@gmail.com>
2019-05-25 00:24:11 +0000

    Return object for save function done

--- public/scripts/initmap.js
+++ public/scripts/initmap.js
@@ -179,15 +179,22 @@ function initMap() {
             const image = $("<section>").addClass("imageURL").append($(`<h2>Image URL</h2><textarea name="text" placeholder="Image URL"></textarea>`));
             const remove = $(`<div id="delete-button"><i class="fa fa-trash"></i> Delete Place :|</div>`);
 
-            const placeHolder = $("<div>").addClass("point hvr-grow").text(pointObject.title);
+            const placeHolder = $("<div>").addClass("point hvr-grow").attr("id", pointObject.id).text(pointObject.title);
             const editBox = $(`<div class="edit-point" style="display: none;"></div>`).append(pointTitle, pointDescription, image, remove);
-            const container = $("<section>").addClass("point-container").append(placeHolder, editBox).appendTo("div.locations");
+            const container = $("<section>").addClass("point-container").attr("id", pointObject.lat).append(placeHolder, editBox).appendTo("div.locations");
           });
         }
 
         function loadMap(mapObject){
           $.get("/map", renderPage(mapObject));
         }
+
+        // function loadMap(){
+        //   $.get("/map", function(data){
+        //     renderPage(data);
+        //     var data = data;
+        //   });
+        // }
 
         const mapObject = {
             "mapInfo": [
@@ -260,79 +267,63 @@ function initMap() {
           }
         });
 
+        function check(val, ref){
+            if(val){
+              return val;
+            }
+            return ref;
+          }
+
         $(document).on( "click", "button.save-button", function(event) {
           event.preventDefault();
           const mapName = $(".edit-title input[name=maptitle]").val();
           const mapDescription = $(".edit-mapdescription textarea[name=text]").val();
           const points = [];
+          const oldPointArray = data.points;
 
           markers.forEach(marker => {
+            var index = 0;
+            for(let i = 0; i < oldPointArray.length; i++){
+              if(marker.lat() == oldPointArray[i].lat){
+                index = i;
+                break;
+              }
+            }
+            const pointID = $("#" + marker.lat()).find("div.point.hvr-grow").attr("id");
             const pointTitle = $("#" + marker.lat()).find(".edit-title input[name=pointtitle]").val();
+            const oldTitle = $("#" + marker.lat()).find("div.point.hvr-grow").text();
             const pointDescription = $("#" + marker.lat()).find(".edit-description textarea[name=text]").val();
+            const oldDescription = ("#" + marker.lat()).find("div.point.hvr-grow").text();
             const pointImage = $("#" + marker.lat()).find(".imageURL textarea[name=text]").val();
+
             const pointObject = {
-              title: pointTitle,
-              image: pointImage,
+              title: check(pointTitle, oldPointArray[index].title),
+              image: check(pointImage, oldPointArray[index].image),
               editorid: userid,
-              description: pointDescription,
+              description: check(pointDescription, oldPointArray[index].description),
               lat: marker.lat(),
               lng: marker.lng()
             };
+            if(pointID){
+              pointObject.id = pointID;
+            }
             points.push(pointObject);
           })
 
-          $.get("/map", function(data){
-            var data = data;
-          });
+          const object = {
+            "mapInfo": [
+              {
+                "mapid": data.mapInfo[0].mapid,
+                "ownerid": data.mapInfo[0].ownerid,
+                "name": check(mapName, data.mapInfo[0].name),
+                "description": check(mapDescription, data.mapInfo[0].description)
+              }
+            ],
+            "points": points,
+            "userFavorites": data.userFavorites
+          }
 
-          // function check(val, ref){
-          //   if(!val)
-          // }
-
-          // const object = {
-          //   "mapInfo": [
-
-          //   ]
-
-          // }
-
-          const data = {
-                      "mapInfo": [
-                          {
-                              "mapid": 1,
-                              "ownerid": "alice",
-                              "name": "a",
-                              "description": "this is map a"
-                          }
-                      ],
-                      "points": [
-                          {
-                              "id": 29,
-                              "title": "point_a1",
-                              "image": "https://preview.redd.it/udxpo5xhyu811.jpg?width=960&crop=smart&auto=webp&s=d2e1870c7378d7d626c83f7c79a1f0cce0ea36e3",
-                              "editorid": "alice",
-                              "description": "this is point a1",
-                              "lat": 49.246292,
-                              "lng": -123.116226
-                          },
-                          {
-                              "id": 30,
-                              "title": "point_a2",
-                              "image": "https://preview.redd.it/udxpo5xhyu811.jpg?width=960&crop=smart&auto=webp&s=d2e1870c7378d7d626c83f7c79a1f0cce0ea36e3",
-                              "editorid": "charlie",
-                              "description": "this is point a2",
-                              "lat": 49.23,
-                              "lng": -123.10
-                          }
-                      ],
-                      "userFavorites": [
-                          "bob",
-                          "alice"
-                      ]
-                  }
-
-
-          $.put("/api/maps/:mapid/save/", loadMap);
+          $.put("/api/maps/:mapid/save/", object, loadMap);
           $("input").val("");
           $("textarea").val("");
           $( "div.edit" ).slideUp();
